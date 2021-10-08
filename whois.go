94ee22e0cfbc181e65976cf960c90d9f65ab16c5
Ulderico Cirello <uldericofilho@gmail.com>
2021-10-08 08:23:06 -0700

    ipwhois: handle referrals

--- whois.go
+++ whois.go
@@ -28,20 +28,31 @@ func Query(ctx context.Context, rawIP string) (string, error) {
 		return "", errors.New("query is not an IP")
 	}
 	ip := parsedIP.String()
-	ianaResp, err := query(ctx, ianaHost, ip)
+	return recursiveQuery(ctx, ianaHost, ip)
+}
+
+func recursiveQuery(ctx context.Context, whoisHost, ip string) (string, error) {
+	whoisResp, err := query(ctx, whoisHost, ip)
 	if err != nil {
-		return "", fmt.Errorf("cannot read IANA's response: %w", err)
+		return "", fmt.Errorf("cannot read %q response: %w", whoisHost, err)
 	}
 	var finalResponse string
-	if foundCountry := grepInsensitiveFirstLine(ianaResp, "country:"); foundCountry != "" {
-		finalResponse = foundCountry
-	} else if foundRedirect := grepInsensitiveFirstLine(ianaResp, "whois:"); foundRedirect != "" {
-		delegate := strings.TrimSpace(strings.TrimPrefix(foundRedirect, "whois:"))
-		redirectResponse, err := query(ctx, delegate, ip)
+	if foundReferral := grepInsensitiveFirstLine(whoisResp, "referralserver:"); foundReferral != "" {
+		delegate := strings.TrimSpace(strings.TrimPrefix(foundReferral, "referralserver:"))
+		referralResponse, err := recursiveQuery(ctx, delegate, ip)
 		if err != nil {
 			return "", fmt.Errorf("cannot read response of delegate %q: %w", delegate, err)
 		}
-		finalResponse = grepInsensitiveFirstLine(redirectResponse, "country:")
+		return referralResponse, nil
+	} else if foundRedirect := grepInsensitiveFirstLine(whoisResp, "whois:"); foundRedirect != "" {
+		delegate := strings.TrimSpace(strings.TrimPrefix(foundRedirect, "whois:"))
+		redirectResponse, err := recursiveQuery(ctx, delegate, ip)
+		if err != nil {
+			return "", fmt.Errorf("cannot read response of delegate %q: %w", delegate, err)
+		}
+		return redirectResponse, nil
+	} else if foundCountry := grepInsensitiveFirstLine(whoisResp, "country:"); foundCountry != "" {
+		finalResponse = foundCountry
 	}
 	resp := strings.TrimSpace(strings.TrimPrefix(finalResponse, "country:"))
 	if resp == "" {
@@ -50,20 +61,21 @@ func Query(ctx context.Context, rawIP string) (string, error) {
 	return resp, nil
 }
 
-func query(ctx context.Context, target, ip string) (string, error) {
+func query(ctx context.Context, whois, ip string) (string, error) {
+	whois = strings.ReplaceAll(whois, "whois://", "")
 	var d net.Dialer
-	conn, err := d.DialContext(ctx, "tcp", net.JoinHostPort(target, "43"))
+	conn, err := d.DialContext(ctx, "tcp", net.JoinHostPort(whois, "43"))
 	if err != nil {
-		return "", fmt.Errorf("cannot talk to %q: %w", target, err)
+		return "", fmt.Errorf("cannot talk to %q: %w", whois, err)
 	}
 	defer conn.Close()
-	query := queryBuilder(target, ip)
+	query := queryBuilder(whois, ip)
 	if _, err := fmt.Fprint(&cancellableWriter{ctx, conn}, query, "\r\n"); err != nil {
-		return "", fmt.Errorf("cannot write query to %q: %w", target, err)
+		return "", fmt.Errorf("cannot write query to %q: %w", whois, err)
 	}
 	var buf bytes.Buffer
 	if _, err := io.Copy(&cancellableWriter{ctx, &buf}, conn); err != nil {
-		return "", fmt.Errorf("cannot read target's response (%s): %w", target, err)
+		return "", fmt.Errorf("cannot read target's response (%s): %w", whois, err)
 	}
 	return buf.String(), nil
 }
